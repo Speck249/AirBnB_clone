546a36456453c982453f6d3f73d9ba9e35baac61
JeffreyKirigo <jeffreyem99@gmail.com>
2023-05-09 23:43:54 +0300

    Models: Added subclasses representing entities

---
+++ models/city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+City Model
+"""
+from models import BaseModel
+
+class City(BaseModel):
+    """City Instance
+
+    Att:
+        state_id (str): state Id
+        name (str): City name
+
+        """
+
+        state_id, name = "", ""
---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+State Class
+"""
+from models import BaseModel
+
+class State(BaseModel):
+    """State Instance
+
+    Att:
+        name (str): state name
+
+    """
+    name = ""
