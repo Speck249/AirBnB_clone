8f2c775991947af579d7a644631a879923f945a3
rediet <redietkfle@gmail.com>
2023-05-10 04:59:57 +0000

    Updated module import statement

--- models/state.py
+++ models/state.py
@@ -2,13 +2,14 @@
 """
 State Class
 """
-from models import BaseModel
+from models.base_model import BaseModel
+
 
 class State(BaseModel):
     """State Instance
 
     Att:
         name (str): state name
-
     """
+
     name = ""
