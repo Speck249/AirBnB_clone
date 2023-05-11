71baa360433a80314b4849b2e31cefad14be0ba8
rediet <redietkfle@gmail.com>
2023-05-11 14:17:16 +0000

    City Test Case

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module presents unittests for
+class City."""
+import unittest
+from models.base_model import BaseModel
+from models.city import City
+import time
+from datetime import datetime
+
+
+class TestCity(unittest.TestCase):
+    """Handles city test cases."""
+
+
+
+if __name__ == '__main__':
+    unittest.main()
