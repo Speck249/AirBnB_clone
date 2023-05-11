36a88fd8f9d9f74f061baf5bfb8b59d6ae849d66
rediet <redietkfle@gmail.com>
2023-05-11 14:25:40 +0000

    BaseModel unittest

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module presents unittests for
+class BaseModel."""
+import unittest
+from models.base_model import BaseModel
+import time
+from datetime import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+    """Handles BaseModel test cases."""
+
+
+
+if __name__ == '__main__':
+    unittest.main()
