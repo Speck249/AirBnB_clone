a21031521f8911fa8d58bbadef46e1f214e70888
rediet <redietkfle@gmail.com>
2023-05-11 15:23:43 +0000

    Unittest Cases

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,7 +2,9 @@
 """Module presents unittests for
 FileStorage class."""
 import unittest
-from models.engine.file_storage import Storage
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
 import time
 from datetime import datetime
 
