a4cd62416c38ef04922c536cc6b52f797951cbb1
rediet <redietkfle@gmail.com>
2023-05-11 14:18:11 +0000

    Place Test Case

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module presents unittests for
+class Place."""
+import unittest
+from models.base_model import BaseModel
+from models.place import Place
+import time
+from datetime import datetime
+
+
+class TestPlace(unittest.TestCase):
+    """Handles place test cases."""
+
+
+if __name__ == '__main__':
+    unittest.main()
