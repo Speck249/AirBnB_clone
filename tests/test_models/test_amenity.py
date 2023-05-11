3e70347591554dd3469904cbfea3b46be4e8e67c
rediet <redietkfle@gmail.com>
2023-05-11 14:15:15 +0000

    Amenity Test Case

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module presents unittests for
+class Amenity."""
+import unittest
+from models.base_model import BaseModel
+from models.amenity import Amenity
+import time
+from datetime import datetime
+
+
+class TestAmenity(unittest.TestCase):
+    """Handles amenity test cases."""
+
+
+
+if __name__ == '__main__':
+    unittest.main()
