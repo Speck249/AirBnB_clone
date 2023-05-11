7cfc324bf0336ebb3e25d0cb6405b09aaa0a2ae2
rediet <redietkfle@gmail.com>
2023-05-11 14:26:16 +0000

    State unittest

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module presents unittests for
+class State."""
+import unittest
+from models.base_model import BaseModel
+from models.state import State
+import time
+from datetime import datetime
+
+
+class TestState(unittest.TestCase):
+    """Handles state test cases."""
+
+
+
+if __name__ == '__main__':
+    unittest.main()
