4f3fe19a190787f52e45b6b98b13425a16fe2ab0
rediet <redietkfle@gmail.com>
2023-05-11 14:25:58 +0000

    Review unittest

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module presents unittests for
+class Review."""
+import unittest
+from models.base_model import BaseModel
+from models.review import Review
+import time
+from datetime import datetime
+
+
+class TestReview(unittest.TestCase):
+    """Handles review test cases."""
+
+
+if __name__ == '__main__':
+    unittest.main()
