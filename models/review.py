b6f1c47f6cd5c2696413b93ae82c080b57357119
rediet <redietkfle@gmail.com>
2023-05-11 13:56:53 +0000

    Update to docstring

--- models/review.py
+++ models/review.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""
-Review Model
-"""
+"""Module presents class Review."""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Review Instance
+    """Class inherits from BaseModel.
+    Handles review instances.
 
     Att:
         place_id (str): Place Id
