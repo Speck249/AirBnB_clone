e1c2cd757008871d6646514ab4866b1e2b1f6b5a
rediet <redietkfle@gmail.com>
2023-05-11 14:26:34 +0000

    User unittest

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module presents unittests for
+User class."""
+import unittest
+from models.base_model import BaseModel
+from models.user import User
+import time
+from datetime import datetime
+
+
+class TestUser(unittest.TestCase):
+    """Handles user test cases."""
+
+
+
+if __name__ == '__main__':
+    unittest.main()
