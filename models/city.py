3986476cd8048a0bb3892dd8ce45af360d7b21b2
rediet <redietkfle@gmail.com>
2023-05-12 08:05:18 +0000

    Update for pycodestyle

--- models/city.py
+++ models/city.py
@@ -4,10 +4,7 @@ City Model
 """
 from models.base_model import BaseModel
 
-<<<<<<< HEAD
 
-=======
->>>>>>> bacd9bbf2117e1367a14d62bac0ad1ae0f155107
 class City(BaseModel):
     """Class inherits from BaseModel.
     Handles city instances.
@@ -15,11 +12,6 @@ class City(BaseModel):
     Att:
         state_id (str): state Id
         name (str): City name
-<<<<<<< HEAD
     """
 
-=======
-
-    """
->>>>>>> bacd9bbf2117e1367a14d62bac0ad1ae0f155107
     state_id, name = "", ""
