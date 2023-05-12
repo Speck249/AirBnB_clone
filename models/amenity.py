3cf7cfad0cac993e3d807dcd7685dd35fab7770d
rediet <redietkfle@gmail.com>
2023-05-12 08:04:58 +0000

    Update for pycodestyle

--- models/amenity.py
+++ models/amenity.py
@@ -6,13 +6,8 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-<<<<<<< HEAD
     """Class inherits from BaseModel.
     Handles Amenity Instance.
-=======
-
-    """Amenity Instance
->>>>>>> bacd9bbf2117e1367a14d62bac0ad1ae0f155107
 
     Att:
         name (str): amenity name
