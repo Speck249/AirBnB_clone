d7ab819950f3bc5b2f822c6836492c309fc4be3a
rediet <redietkfle@gmail.com>
2023-05-12 08:06:39 +0000

    Update for pycodestyle

--- models/user.py
+++ models/user.py
@@ -4,10 +4,7 @@ User class
 """
 from models.base_model import BaseModel
 
-<<<<<<< HEAD
 
-=======
->>>>>>> bacd9bbf2117e1367a14d62bac0ad1ae0f155107
 class User(BaseModel):
     """Class inherits from BaseModel.
     Handles user instances.
@@ -18,9 +15,6 @@ class User(BaseModel):
         first_name (str): 1st name
         last_name (str): last name
     """
-<<<<<<< HEAD
 
-=======
->>>>>>> bacd9bbf2117e1367a14d62bac0ad1ae0f155107
     email, password = "", ""
     first_name, last_name = "", ""
