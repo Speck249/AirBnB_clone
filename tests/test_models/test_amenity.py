dbac2eb8209660b5fe5f317070031af80a5c48d5
rediet <redietkfle@gmail.com>
2023-05-12 11:04:03 +0000

    Test suite for Amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -3,22 +3,47 @@
 import unittest
 from models.base_model import BaseModel
 from models.amenity import Amenity
-from datetime import datetime
+import datetime
 import time
 
 
 class TestAmenity(unittest.TestCase):
     """Handles amenity test cases."""
-
     obj = Amenity()
 
-    def setUp():
-        """Prepares test fixtures."""
-        pass
-
     def test_subclass_inheritance(self):
-        """Tests if amenity inherits from BaseModel."""
-        self.assertIsInstance(self.obj, Amenity)
+        """Assert subclass inheritance from base class."""
+        self.assertTrue(issubclass(Amenity, BaseModel))
+
+    def test_for_polymorphism(self):
+        """Assert interchangeability between base & subclass."""
+        a =  BaseModel()
+        assert isinstance(Amenity("Hospitals"), BaseModel)
+        assert not isinstance(Amenity("Hospitals"), str)
+        assert not isinstance(a, Amenity)
+
+    def test_subclass_attributes_exist(self):
+        """Assert subclass attributes exist."""
+        self.assertTrue(hasattr(self.obj, 'id'))
+        self.assertTrue(hasattr(self.obj, 'name'))
+        self.assertTrue(hasattr(self.obj, 'created_at'))
+        self.assertTrue(hasattr(self.obj, 'updated_at'))
+
+    def test_subclass_attribute_type(self):
+        """Assert subclass attribute types."""
+        msg = "instance type error."
+        self.assertIsInstance(self.obj.id, str, msg)
+        self.assertIsInstance(self.obj.name, str, msg)
+        self.assertIsInstance(self.obj.created_at, datetime.datetime, msg)
+        self.assertIsInstance(self.obj.updated_at, datetime.datetime, msg)
+
+    def test_subclass_exceptions(self):
+        """Assert exception handling."""
+        b = Amenity("Schools")
+        try:
+            b.name = "name type error"
+        except Exception as e:
+            assert isinstance(e, TypeError)
 
 if __name__ == '__main__':
     unittest.main()
