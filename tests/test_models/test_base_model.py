bdcd83301a07f33c9118b2cd3981b0828f6cb16c
rediet <redietkfle@gmail.com>
2023-05-13 18:17:14 +0000

    Test suite for BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,19 +1,96 @@
 #!/usr/bin/python3
-"""Module presents unittests for
-class BaseModel."""
+"""Unit test suite for BaseModel class."""
 import unittest
+import models
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-from models import storage
-import uuid
-import time
 from datetime import datetime
+import time
 
 
 class TestBaseModel(unittest.TestCase):
-    """Handles BaseModel test cases."""
+    """Handles base_model test cases."""
+    obj = BaseModel()
 
+    def test_basemodel_instantiation(self):
+        """Assert parent class constructor."""
+        unique_id = str(uuid.uuid4())
+        iso_ts = datetime.today().isoformat()
+        obj_b = BaseModel(id=unique_id, created_at=iso_ts,
+                  updated_at=iso_ts)
+        self.assertEqual(obj_b.id, unique_id)
+        self.assertEqual(obj_b.created_at, iso_ts)
+        self.assertEqual(obj_b.updated_at, iso_ts)
 
+    def test_uuid_exists(self):
+        """Assert base_model has public instance id."""
+        self.assertTrue(hasattr(self.obj, 'id'))
+
+    def test_created_at_exists(self):
+        """Assert base_model has public instance created_at."""
+        self.assertTrue(hasattr(self.obj, 'created_at'))
+
+    def test_updated_id_exists(self):
+        """Assert base model has public instance updated_at."""
+        self.assertTrue(hasattr(self.obj, 'updated_at'))
+
+    def test_basemodel_uuid_type(self):
+        """Assert attribute type."""
+        self.assertIsInstance(self.obj.id, str)
+
+    def test_basemodel_datetime_type(self):
+        """Assert attribute type."""
+        self.assertEqual(datetime, type(self.obj.created_at))
+
+    def test_basemodel_datetime_type(self):
+        """Assert attribute type."""
+        self.assertEqual(datetime, type(self.obj.updated_at))
+
+    def test_basemodel_uuid(self):
+        """Assert unique id generation."""
+        b_obj = BaseModel()
+        obj_b = BaseModel()
+        self.assertNotEqual(b_obj, obj_b)
+
+    def test_save(self):
+        obj_b = BaseModel()
+        time.sleep(0.12)
+        initial_update = obj_b.update_at
+        obj_b.save()
+        self.assertLess(initial_update, obj_b.updated_at)
+
+    def test_to_dict_keys(self):
+        """Assert accuracy of keys inside dict"""
+        obj_b = BaseModel()
+        self.assertIn("__class__", obj_b.to_dict())
+        self.assertIn("id", obj_b.to_dict())
+        self.assertIn("created_at", obj_b.to_dict())
+        self.assertIn("updated_at", obj_b.to_dict())
+
+    def test_to_dict_attributes(self):
+        """Assert attribute values within dict."""
+        obj_b = Amenity()
+        obj_b.name = "My First Model"
+        obj_b.my_number = 89
+        self.assertEqual("My First Model", obj_b.name)
+        self.assertIn("my_number", obj_b.to_dict())
+
+    def test_to_dict(self):
+        """Assert expected method output."""
+        msg = "Unexpected output."""
+        obj_b = BaseModel()
+        ts = datetime.today()
+        ts_iso = ts.isoformat()
+        obj_b.id = "74cb9cb3-bb94-438b-af2b-7889aef86a8d"
+        obj_b.created_at = ts
+        obj_b.updated_at = ts
+        test_dict = {
+            "__class__": "BaseModel",
+            "id": "74cb9cb3-bb94-438b-" \
+            "af2b-7889aef86a8d",
+            "created_at": ts,
+            "updated_at": ts,
+        }
+        self.assertDictEqual(test_dict, obj_b.to_dict(), msg)
 
 if __name__ == '__main__':
     unittest.main()
