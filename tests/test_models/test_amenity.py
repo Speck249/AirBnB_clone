a4202b698f56a70ffebe95c93e308dcb1cdb3ff3
rediet <redietkfle@gmail.com>
2023-05-13 19:29:05 +0000

    Test suite: Amenity class

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -57,7 +57,7 @@ class TestAmenity(unittest.TestCase):
     def test_save(self):
         obj_a = Amenity()
         time.sleep(0.12)
-        initial_update = obj_a.update_at
+        initial_update = obj_a.updated_at
         obj_a.save()
         self.assertLess(initial_update, obj_a.updated_at)
 
@@ -81,18 +81,18 @@ class TestAmenity(unittest.TestCase):
         """Assert expected method output."""
         msg = "Unexpected output."""
         obj_a = Amenity()
+        obj_a.id = "74cb9cb3"
         ts = datetime.today()
-        ts_iso = ts.isoformat()
-        obj_a.id = "74cb9cb3-bb94-438b-af2b-7889aef86a8d"
         obj_a.created_at = ts
         obj_a.updated_at = ts
+        ts_iso = ts.isoformat()
         test_dict = {
             "__class__": "Amenity",
-            "id": "74cb9cb3-bb94-438b-" \
-            "af2b-7889aef86a8d",
-            "created_at": ts,
-            "updated_at": ts,
+            "id": "74cb9cb3",
+            "created_at": ts_iso,
+            "updated_at": ts_iso,
         }
+
         self.assertDictEqual(test_dict, obj_a.to_dict(), msg)
 
     def test_subclass_exceptions(self):
