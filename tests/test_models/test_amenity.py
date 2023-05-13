99df9cd2e282d03bbf00618ed25279753d733aa8
rediet <redietkfle@gmail.com>
2023-05-13 08:04:13 +0000

    Test suite for Amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -17,31 +17,33 @@ class TestAmenity(unittest.TestCase):
 
     def test_for_polymorphism(self):
         """Assert interchangeability between base & subclass."""
-        a =  BaseModel()
+        b =  BaseModel()
         assert isinstance(Amenity("Hospitals"), BaseModel)
         assert not isinstance(Amenity("Hospitals"), str)
-        assert not isinstance(a, Amenity)
+        assert not isinstance(b, Amenity)
 
     def test_subclass_attributes_exist(self):
         """Assert subclass attributes exist."""
-        self.assertTrue(hasattr(self.obj, 'id'))
         self.assertTrue(hasattr(self.obj, 'name'))
+        self.assertTrue(hasattr(self.obj, 'id'))
         self.assertTrue(hasattr(self.obj, 'created_at'))
         self.assertTrue(hasattr(self.obj, 'updated_at'))
 
     def test_subclass_attribute_type(self):
         """Assert subclass attribute types."""
         msg = "instance type error."
-        self.assertIsInstance(self.obj.id, str, msg)
         self.assertIsInstance(self.obj.name, str, msg)
-        self.assertIsInstance(self.obj.created_at, datetime.datetime, msg)
-        self.assertIsInstance(self.obj.updated_at, datetime.datetime, msg)
+        self.assertIsInstance(self.obj.id, str, msg)
+        self.assertIsInstance(self.obj.created_at,
+                      datetime.datetime, msg)
+        self.assertIsInstance(self.obj.updated_at,
+                      datetime.datetime, msg)
 
     def test_subclass_exceptions(self):
         """Assert exception handling."""
-        b = Amenity("Schools")
+        amenity = Amenity("Hospitals")
         try:
-            b.name = "name type error"
+            amenity.name = "name type error"
         except Exception as e:
             assert isinstance(e, TypeError)
 
