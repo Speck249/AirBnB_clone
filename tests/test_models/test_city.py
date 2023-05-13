1f4e7914269f684be5c1768d33aff27c1ad974c7
rediet <redietkfle@gmail.com>
2023-05-13 18:16:47 +0000

    Test suite for City class

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Unit test suite for City class."""
 import unittest
+import models
 from models.base_model import BaseModel
 from models.city import City
-import datetime
+from datetime import datetime
 import time
 
 
@@ -15,31 +16,99 @@ class TestCity(unittest.TestCase):
         """Assert subclass inheritance from base class."""
         self.assertTrue(issubclass(City, BaseModel))
 
+    def test_name_attribute_exists(self):
+        """Assert subclass attribute is present."""
+         self.assertTrue(hasattr(self.obj, 'state_id'))
+
+    def test_name_attribute_exists(self):
+        """Assert subclass attribute is present."""
+        self.assertTrue(hasattr(self.obj, 'name'))
+
+    def test_id_attribute_exists(self):
+        """Assert inherited attribute is present."""
+        self.assertTrue(hasattr(self.obj, 'id'))
+
+    def test_created_at_attribute_exists(self):
+        """Assert inherited attribute is present."""
+        self.assertTrue(hasattr(self.obj, 'created_at'))
+
+    def test_updated_at_attribute_exists(self):
+        """Assert inherited attribute exist."""
+        self.assertTrue(hasattr(self.obj, 'updated_at'))
+
+    def test_subclass_attribute_type(self):
+        """Assert subclass attribute types."""
+        self.assertIsInstance(self.obj.state_id, str)
+
+    def test_subclass_attribute_type(self):
+        """Assert subclass attribute types."""
+        self.assertIsInstance(self.obj.name, str)
+
+    def test_subclass_attribute_type(self):
+        """Assert subclass attribute types."""
+        self.assertIsInstance(self.obj.id, str)
+
+    def test_subclass_attribute_type(self):
+        """Assert subclass attribute types."""
+        self.assertEqual(datetime, type(self.obj.created_at))
+
+    def test_subclass_attribute_type(self):
+        """Assert subclass attribute types."""
+        self.assertEqual(datetime, type(self.obj.updated_at))
+
+    def test_inherited_uuid(self):
+        """Assert generated id is unique."""
+        obj_c = City()
+        c_obj = City()
+        self.assertNotEqual(obj_c, c_obj)
+
+    def test_save(self):
+        obj_c = City()
+        time.sleep(0.12)
+        initial_update = obj_c.update_at
+        obj_c.save()
+        self.assertLess(initial_update, obj_c.updated_at)
+
+    def test_to_dict_keys(self):
+        """Assert accuracy of keys inside dict"""
+        obj_c = City()
+        self.assertIn("__class__", obj_c.to_dict())
+        self.assertIn("id", obj_c.to_dict())
+        self.assertIn("created_at", obj_c.to_dict())
+        self.assertIn("updated_at", obj_c.to_dict())
+
+    def test_to_dict_attributes(self):
+        """Assert attribute values within dict."""
+        obj_c = City()
+        obj_c.name = "My First Model"
+        obj_c.my_number = 89
+        self.assertEqual("My First Model", obj_c.name)
+        self.assertIn("my_number", obj_c.to_dict())
+
+    def test_to_dict(self):
+        """Assert expected method output."""
+        msg = "Unexpected output."""
+        obj_c = City()
+        ts = datetime.today()
+        ts_iso = ts.isoformat()
+        obj_c.id = "74cb9cb3-bb94-438b-af2b-7889aef86a8d"
+        obj_c.created_at = ts
+        obj_c.updated_at = ts
+        test_dict = {
+            "__class__": "City",
+            "id": "74cb9cb3-bb94-438b-" \
+            "af2b-7889aef86a8d",
+            "created_at": ts,
+            "updated_at": ts,
+        }
+        self.assertDictEqual(test_dict, obj_c.to_dict(), msg)
+
     def test_for_polymorphism(self):
         """Assert interchangeability between base & subclass."""
         b =  BaseModel()
         assert isinstance(City("Albany"), BaseModel)
         assert not isinstance(City("Albany"), str)
         assert not isinstance(b, City)
-
-    def test_subclass_attributes_exist(self):
-        """Assert subclass attributes exist."""
-        self.assertTrue(hasattr(self.obj, 'state_id'))
-        self.assertTrue(hasattr(self.obj, 'name'))
-        self.assertTrue(hasattr(self.obj, 'id'))
-        self.assertTrue(hasattr(self.obj, 'created_at'))
-        self.assertTrue(hasattr(self.obj, 'updated_at'))
-
-    def test_subclass_attribute_type(self):
-        """Assert subclass attribute types."""
-        msg = "instance type error."
-        self.assertIsInstance(self.obj.state_id, str, msg)
-        self.assertIsInstance(self.obj.name, str, msg)
-        self.assertIsInstance(self.obj.id, str, msg)
-        self.assertIsInstance(self.obj.created_at,
-                      datetime.datetime, msg)
-        self.assertIsInstance(self.obj.updated_at,
-                      datetime.datetime, msg)
 
     def test_subclass_exceptions(self):
         """Assert exception handling."""
