8865be0ab33973d175bb275ed4abac7eb3c84d0a
JeffreyKirigo <jeffreyem99@gmail.com>
2023-05-13 11:21:42 +0300

    Fix: Reload Issue caused by 'AttributeError'

--- models/base_model.py
+++ models/base_model.py
@@ -15,14 +15,14 @@ class BaseModel():
             args: non-keyworded argument list.
             kwargs: keyworded argument list
         """
-
+        tformat = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs != {} and kwargs is not None:
             for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-                elif key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(kwargs[key], 
-                          "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, \
+                            tformat)
+                else:
+                    self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -49,10 +49,8 @@ class BaseModel():
 
         dict_rep = self.__dict__.copy()
         dict_rep["__class__"] = self.__class__.__name__
-        dict_rep["created_at"] = self.created_at.strftime\
-           ("%Y-%m-%dT%H:%M:%S.%f")
-        dict_rep["updated_at"] = self.updated_at.strftime\
-           ("%Y-%m-%dT%H:%M:%S.%f")
+        dict_rep["created_at"] = self.created_at.isoformat()
+        dict_rep["updated_at"] = self.updated_at.isoformat()
 
         return dict_rep
 
