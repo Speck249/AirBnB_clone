523a69e5c7dbb1f4b7b222cb4756e9d5fae43341
rediet <redietkfle@gmail.com>
2023-05-13 18:34:21 +0000

    Test suite: Amenity class

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -48,7 +48,7 @@ class TestAmenity(unittest.TestCase):
         """Assert subclass attribute types."""
         self.assertEqual(datetime, type(self.obj.updated_at))
 
-    def test_basemodel_uuid(self):
+    def test_inherited_id(self):
         """Assert generated id is unique."""
         obj_a = Amenity()
         a_obj = Amenity()
