e611b8fa8eca45fee2dc8afc2395571d1e630f37
rediet <redietkfle@gmail.com>
2023-05-13 18:18:03 +0000

    Test suite for State class

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Unit test suite for State class."""
 import unittest
+import models
 from models.base_model import BaseModel
 from models.state import State
-import datetime
+from datetime import datetime
 import time
 
 
@@ -15,29 +16,76 @@ class TestState(unittest.TestCase):
         """Assert subclass inheritance from base class."""
         self.assertTrue(issubclass(State, BaseModel))
 
-    def test_for_polymorphism(self):
-        """Assert interchangeability between base & subclass."""
-        b =  BaseModel()
-        assert isinstance(State("New York"), BaseModel)
-        assert not isinstance(State("New York"), str)
-        assert not isinstance(b, State)
-
-    def test_subclass_attributes_exist(self):
+    def test_id_attribute_exists(self):
         """Assert subclass attributes exist."""
         self.assertTrue(hasattr(self.obj, 'name'))
+
+    def test_id_attribute_exists(self):
+        """Assert inherited attribute is present."""
         self.assertTrue(hasattr(self.obj, 'id'))
         self.assertTrue(hasattr(self.obj, 'created_at'))
         self.assertTrue(hasattr(self.obj, 'updated_at'))
 
     def test_subclass_attribute_type(self):
         """Assert subclass attribute types."""
-        msg = "instance type error."
-        self.assertIsInstance(self.obj.name, str, msg)
+        self.assertIsInstance(self.obj.name, str)
         self.assertIsInstance(self.obj.id, str, msg)
-        self.assertIsInstance(self.obj.created_at,
-                      datetime.datetime, msg)
-        self.assertIsInstance(self.obj.updated_at,
-                      datetime.datetime, msg)
+        self.assertEqual(datetime, type(self.obj.created_at))
+        self.assertEqual(datetime, type(self.obj.updated_at))
+
+    def test_basemodel_uuid(self):
+        """Assert generated id is unique."""
+        obj_s = State()
+        s_obj = State()
+        self.assertNotEqual(obj_s, s_obj)
+
+    def test_save(self):
+        obj_s = State()
+        time.sleep(0.12)
+        initial_update = obj_s.update_at
+        obj_s.save()
+        self.assertLess(initial_update, obj_s.updated_at)
+
+    def test_to_dict_keys(self):
+        """Assert accuracy of keys inside dict"""
+        obj_s = State()
+        self.assertIn("__class__", obj_s.to_dict())
+        self.assertIn("id", obj_s.to_dict())
+        self.assertIn("created_at", obj_s.to_dict())
+        self.assertIn("updated_at", obj_s.to_dict())
+
+    def test_to_dict_attributes(self):
+        """Assert attribute values within dict."""
+        obj_s = State()
+        obj_s.name = "My First Model"
+        obj_s.my_number = 89
+        self.assertEqual("My First Model", obj_s.name)
+        self.assertIn("my_number", obj_s.to_dict())
+
+    def test_to_dict(self):
+        """Assert expected method output."""
+        msg = "Unexpected output."""
+        obj_s = State()
+        ts = datetime.today()
+        ts_iso = ts.isoformat()
+        obj_s.id = "74cb9cb3-bb94-438b-af2b-7889aef86a8d"
+        obj_s.created_at = ts
+        obj_s.updated_at = ts
+        test_dict = {
+            "__class__": "State",
+            "id": "74cb9cb3-bb94-438b-" \
+            "af2b-7889aef86a8d",
+            "created_at": ts,
+            "updated_at": ts,
+        }
+        self.assertDictEqual(test_dict, obj_s.to_dict(), msg)
+
+    def test_for_polymorphism(self):
+        """Assert interchangeability between base & subclass."""
+        b =  BaseModel()
+        assert isinstance(State("New York"), BaseModel)
+        assert not isinstance(State("New York"), str)
+        assert not isinstance(b, State)
 
     def test_subclass_exceptions(self):
         """Assert exception handling."""
