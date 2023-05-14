0da1c2393898020a043f3f95bd9bdf88ae5b39d8
rediet <redietkfle@gmail.com>
2023-05-14 18:04:58 +0000

    Hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -40,7 +40,6 @@ class BaseModel():
         """Method updates attribute updated_at."""
 
         self.updated_at = datetime.now()
-        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
