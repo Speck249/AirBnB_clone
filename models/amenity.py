7ec0fbbece9343db3bd1ed61e6a513b6eaf96fc7
JeffreyKirigo <jeffreyem99@gmail.com>
2023-05-14 18:03:48 +0300

    Conflicts in master branch resolved

--- models/amenity.py
+++ models/amenity.py
@@ -2,25 +2,14 @@
 """
 Amenity Model
 """
-<<<<<<< HEAD
-from models import BaseModel
-
-class City(BaseModel):
-=======
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
->>>>>>> d6106ffb420be867ae15e410a0841f35099b0380
     """Amenity Instance
 
     Att:
         name (str): amenity name
-<<<<<<< HEAD
-
-        """
-=======
     """
->>>>>>> d6106ffb420be867ae15e410a0841f35099b0380
 
-        name = ""
+    name = ""
